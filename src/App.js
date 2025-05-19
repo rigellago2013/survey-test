e48e1e975fcf9ad1c8ea78a9a40a015b2448c3df
rigellago2013 <31771558+rigellago2013@users.noreply.github.com>
2025-05-20 00:51:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -80,21 +80,6 @@ const App = () => {
   
   return (
     <div className="min-h-screen bg-white">
-      {/* Top Header Bar */}
-      <div className="bg-blue-900 text-white py-2">
-        <div className="container mx-auto px-4 flex justify-between items-center">
-          <div>Your Quality Is Our Concern</div>
-          <div className="flex space-x-4 text-sm">
-            <a href="#" className="hover:underline">About us</a>
-            <a href="#" className="hover:underline">MAZEN</a>
-            <a href="#" className="hover:underline">News</a>
-            <a href="#" className="hover:underline">Contact us</a>
-            <a href="#" className="hover:underline">Events Calendar</a>
-            <a href="#" className="hover:underline">Careers</a>
-            <a href="#" className="hover:underline"><svg className="w-4 h-4 inline" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
-          </div>
-        </div>
-      </div>
     
       {/* Main Navigation */}
 <div className="bg-gray-100 border-b border-gray-200">
